d267267d1a7912bf79077be95133eb97cea318e9
Rajput Aniruddhsinh <aniruddhrajput471@gmail.com>
2026-03-16 21:53:03 +0530

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-100 text-center text-sm  py-4 shadow-inner border-t border-gray-300">
-      © {new Date().getFullYear()} FoodoraX. All rights reserved.
+    <footer className="bg-green-100 text-center text-sm py-4 shadow-inner border-t border-gray-300">
+      <p className="text-gray-600">
+        Created by{" "}
+        <span className="font-bold text-green-700 hover:text-green-900 hover:scale-110 transition-transform duration-300 inline-block cursor-pointer">
+          Aniruddh
+        </span>
+      </p>
+      <p>© {new Date().getFullYear()} FoodoraX. All rights reserved.</p>
     </footer>
   );
 };
